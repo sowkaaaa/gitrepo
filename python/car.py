8842d5db368a60e395319960f9b3232a7a4caac2
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2018-12-11 13:31:22 +0100

    car cd.

--- python/car.py
+++ python/car.py
@@ -9,20 +9,30 @@
 # rok produkcji, np . 2002
 # metoda wiek(), która zwróci wiek samochodu w latach
 from datetime import date
+
 from osoba_obj import Osoba
 
 class Kierowca(Osoba):
     
+    __licznik = 0  #atrybut klasy, współdzielony przez obiekty
+    
     def __init__(self, imie, nazwisko, kategoria):
         super().__init__(imie, nazwisko)
         self.kategoria = kategoria
+        Kierowca.__licznik += 1
+        
+    def __del__(self):
+        Kierowca.__licznik -= 1
+        
+    @staticmethod # metoda statyczna
+    def KierowcaLicz():
+        return Kierowca.__licznik
 
 class Samochod():
-    def __init__(self, marka, model, rokProdukcji):
+    def __init__(self, marka, model, rocznik):
         self.marka = marka
         self.model = model
-        self.rokProdukcji = rokProdukcji
-        self.kierowca = kierowca
+        self.rok = rocznik
         
     def wiek(self):
         dzis = date.today()
@@ -32,11 +42,26 @@ class Samochod():
 
 
 def main(args):
-    s1 = Samochod('Fiat', 'Punto', '2000')
-    s2 = Samochod('Toyota', 'Auris', '2015')
+    s1 = Samochod('Fiat', 'Punto', 2000)
+    s2 = Samochod('Toyota', 'Auris', 2015)
     
-    print(s1.marka, s1.model, s1.rokProdukcji, s1.wiek())
-    print(s2.marka, s2.model, s2.rokProdukcji, s2.wiek())
+    print(s1.marka, s1.model, s1.rok, s1.wiek())
+    print(s2.marka, s2.model, s2.rok, s2.wiek())
+    
+    k1 =  Kierowca('Adam', 'Słodowy', 'B')
+    print(k1.imie, k1.kategoria)
+    print(k1.KierowcaLicz())
+    k2 =  Kierowca('Ewa', 'Bąk', 'BC')
+    print(k1.KierowcaLicz())
+    k3 =  Kierowca('Bartek', 'Dąb', 'BC')
+    print(k1.KierowcaLicz())
+    print(k2.KierowcaLicz())
+    print(k3.KierowcaLicz())
+    k3.licznik = 10
+    del(k2)
+    del(k3)
+    print(k1.KierowcaLicz())
+    
     return 0
 
 if __name__ == '__main__':
