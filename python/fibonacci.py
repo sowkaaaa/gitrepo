6ede1cc4c2a992fb03f0e4f332b494a8309faa37
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-02-12 13:31:21 +0100

    fibonacci cd

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -8,14 +8,35 @@
 
 def czy_naturalne(n):
     if n.isdigit():
-        return int(a)
+        return True
     return False
-
+    
+def fib_it(n):
+    if n < 2:
+        return n
+    a, b, wynik = 0, 1, 0
+    
+    for i in range(2, n + 1):
+        if a > 0:
+            print ("{}/{} = {}".format(b, a, b / a))
+        wynik = a + b
+        a, b = b, wynik
+    return wynik
+    
+    
 def main(args):
-    n = input('Który wyraz ciagu?')
+    #assert fib_it(0) == 0
+    #assert fib_it(1) == 1
+    #assert fib_it(7) == 13
+    assert fib_it(19) == 4181
+    
+    n = input('Który wyraz ciagu? ')
     while not czy_naturalne(n):
         print('Błędne dane!')
-        n = input('Który wyraz ciagu?')
+        n = input('Który wyraz ciągu? ')
+    
+    print("F_it({}) = {}".format(n, fib_it(int(n))))
+        
     return 0
 
 if __name__ == '__main__':
