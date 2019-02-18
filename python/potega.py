08e065c7c64c943463324fc5c5bc7d3550a26810
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-02-18 12:36:14 +0100

    rekurencja

--- python/potega.py
+++ python/potega.py
@@ -15,21 +15,26 @@ def potega_it(a, n):
     for i in range(n):
         wynik = wynik * a
     return wynik
+    
+def potega_rek(a, n):
+    if n == 0:
+        return 1
+    return potega_rek(a, n-1) * a
 
 
 def main(args):
-    assert potega_it(2, 0) == 1
-    assert potega_it(2, 1) == 2
-    assert potega_it(0, 10) == 0
-    assert potega_it(1, 10) == 1
-    assert potega_it(3, 3) == 27
+    assert potega_rek(2, 0) == 1
+    assert potega_rek(2, 1) == 2
+    assert potega_rek(0, 10) == 0
+    assert potega_rek(1, 10) == 1
+    assert potega_rek(3, 3) == 27
     
     a = input('Podaj podstawę:')
     n = input('Podaj wykładnik:')
     if not czy_naturalna(a) or not czy_naturalna(n):
         print("Błędne dane!")
         return 0
-    print("{} do potęgi {} = {}".format(a, n, potega_it(int(a), int(n))))
+    print("{} do potęgi {} = {}".format(a, n, potega_rek(int(a), int(n))))
     
     return 0
 
--- python/silnia.py
+++ python/silnia.py
@@ -13,13 +13,31 @@ def silnia_it(n):
     for i in range(1, n + 1):
         wynik = wynik * i
     return wynik
+    
+def silnia_rek(n):
+    # n! = (n-1)! * n 
+    # 4! = 3! * 4
+    # 3! = 2! * 3 
+    # 2! = 1! * 2
+    # 1! = 0! * 1
+    # 0! = 1
+    # 1! = 1 * 1
+    # 2! = 1 * 2
+    # 3! = 2 * 3
+    # 4! = 6 * 4       
+    if n == 0:
+        return 1
+    return silnia_rek(n-1) * n
+        
+    
+    
 
 def main(args):
-    assert silnia_it(5) == 120
-    assert silnia_it(1) == 1
+    assert silnia_rek(5) == 120
+    assert silnia_rek(1) == 1
     
     n = int(input('Podaj liczbę naturalną:'))
-    print("{}! = {}".format(n, silnia_it(n)))
+    print("{}! = {}".format(n, silnia_rek(n)))
     return 0
 
 if __name__ == '__main__':
