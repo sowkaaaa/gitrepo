9802822abe03a82e205d887ab5019fb61297c5e8
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-03-05 13:30:43 +0100

    tlumacz

--- python/palindromy.py
+++ python/palindromy.py
@@ -26,9 +26,12 @@ def main(args):
     teksty = czytaj_dane('dane01.txt')
     print(teksty)
     ile = 0
-    for teksty in range(teksty):
-        if czy_palindrom(teksty):
-            
+    for i in range (len(teksty)):
+        if czy_palindrom(teksty[i]):
+            ile += 1
+        else:
+            continue
+    print(ile)
     # ~if czy_palindrom(tekst):
         # ~print('To palindrom!')
     # ~else:
