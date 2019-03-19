5f2fdedf8c643786caa3acdabb132993b481d5ed
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-03-19 13:33:08 +0100

    wykresy

--- python/wykres02.py
+++ python/wykres02.py
@@ -7,12 +7,21 @@ from matplotlib.mlab import frange
 
 
 def main(args):
-    # y = 
     x = frange(-1, 2, 0.15)
-    y = [a * i + b for i in x]
+    # f(x) = x / (x+2) dla x >= 1
+    # f(x) = x * x/3 dla x > 0 i x < 1
+    # f(x) = x /-3 dla x <= 0
+    y = []
+    for el in x:
+        if el <= 0:
+            y.append(el / -3)
+        elif el < 1:
+            y.append(el * el / 3)
+        else:
+            y.append(el / (el + 2))
     
     plt.plot(x, y)
-    plt.title('Wykres f(x) = {}*x + {}'.format(a, b))
+    plt.title('Wykres f(x) = {}*x + {}')
     plt.grid(True)
     plt.show()
     return 0
@@ -20,3 +29,4 @@ def main(args):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+ 
