c8cee28007e0e36f930608673012df0a8b40ed54
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-09-17 19:34:25 +0200

    plan

--- cpp/program1.cpp
+++ cpp/program1.cpp
@@ -4,16 +4,16 @@
 
 
 #include <iostream>
- 
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
 	int  liczba1;
 	float liczba2;
-	cout << "Podaj liczbę całkowitą: ";
+	cout << "Podaj liczbe calkowita: ";
 	cin >> liczba1;
-	cout << "Podałeś: " << liczba1;  
+	cout << "Podales: " << liczba1;
 	return 0;
 }
 
