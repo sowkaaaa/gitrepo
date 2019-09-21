e4f9268e1c75aecedb6cd5d186845a8c00b92ad0
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-09-21 19:29:35 +0200

    bs4

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -1,6 +1,6 @@
 /*
  * trojkat.cpp
- * 
+ *
  * Napisz program, który pobiera kod użytkownika i sprawdza czy da sie z nich budować trójkąt.
  * Program powinien wyprowadzać odpowiedni komunikat.
  */
@@ -12,23 +12,27 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	int bok1;
+	int a;
 	cout << "Podaj bok pierwszy: ";
-	cin >> bok1;
-	int bok2;
+	cin >> a;
+	int b;
 	cout << "Podaj bok drugi: ";
-	cin >> bok2;
-	int bok3;
+	cin >> b;
+	int c;
 	cout << "Podaj bok trzeci: ";
-	cin >> bok3;
-	if (bok1 + bok2 > bok3)cout << "To jest trójkat";
-		else cout << "To trójkątem na pewno nie jest!";
-		
-	if (sqrt(bok1)+ sqrt(bok2) = sqrt(bok3)) cout << "Trójkat prostokątny"
-		else cout << "To nie jest trójkąt prostokątny"
-	
-	
-	
+	cin >> c;
+	if (a+b>c && a+c>b && b+c >a)
+        cout << "To jest trójkat" << endl;
+    else
+		cout << "To trójkątem na pewno nie jest!" << endl;
+    if (sqrt(a)+ sqrt(b) = sqrt(c))
+        cout << "Trójkat prostokątny" << endl;
+    else
+        cout << "To nie jest trójkąt prostokątny" << endl;
+
+
+
+
 	return 0;
 }
 
