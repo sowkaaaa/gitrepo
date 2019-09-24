129aea2d125dc18d2462c6a6da1ac1506a3bc5ac
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-09-24 20:42:00 +0200

    cpp

--- cpp/liczby/main.cpp
+++ cpp/liczby/main.cpp
@@ -13,33 +13,33 @@ int main()
 	int c;
 	cout << "Podaj liczbę trzecia: " << endl;
 	cin >> c;
-    if (a > b && a > c) 
-    {cout << a << "" << "najwieksza" << endl;}
-		 if (b > c && b < a)
-		 {cout << b << "" << "średnia" << endl;}
-		 else
-		 if (c > b && c < a)
-		 {cout << c << "" << "średnia" << endl;} 
-		   if (c < a &&  c < b)
-		   {cout << c << "" << "njmniejsza" << endl;}
-	if (b > a && b > c) 
-    {cout << b << "" << "najwieksza" << endl;}
-		 if (c > a && c < b)
-		 {cout << c << "" << "średnia" << endl;}
-		 else
-		 if (a > c && a < b)
-		 {cout << a << "" << "średnia" << endl;} 
-		   if (a < b &&  a < c)
-		   {cout << a << "" << "njmniejsza" << endl;}
-	if (c > a && c > b) 
-    {cout << c << "" << "najwieksza" << endl;}
-		 if (a > b && a < c)
-		 {cout << a << "" << "średnia" << endl;}
-		 else
-		 if (b > a && b < c)
-		 {cout << b << "" << "średnia" << endl;} 
-		   if (b < a &&  b < c)
-		   {cout << b << "" << "njmniejsza" << endl;}
+    if (a > b && a > c)
+        {cout << a << "najwieksza" << endl;}
+        if (b > c && b < a)
+        {cout << b << "średnia" << endl;}
+        else
+        if (c > b && c < a)
+        {cout << c << "średnia" << endl;}
+        if (c < a &&  c < b)
+        {cout << c << "njmniejsza" << endl;}
+	if (c > a && c > b)
+        {cout << c << "najwieksza" << endl;}
+        if (a > b && a < c)
+        {cout << a << "średnia" << endl;}
+        else
+        if (b > a && b < c)
+        {cout << b << "średnia" << endl;}
+        if (b < a &&  b < c)
+        {cout << b << "njmniejsza" << endl;}
+    if (b > a && b > c)
+        {cout << b << "najwieksza" << endl;}
+        if (c > a && c < b)
+        {cout << c << "średnia" << endl;}
+        else
+        if (a > c && a < b)
+        {cout << a << "średnia" << endl;}
+        if (a < b &&  a < c)
+        {cout << a << "njmniejsza" << endl;}
 
     return 0;
 }
