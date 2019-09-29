f8da5208e37c245cfa53b6a9883549cafca8e1e9
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-09-29 14:44:09 +0200

    petle-cwiczenia

---
+++ cpp/petla_cw4.cpp
@@ -0,0 +1,17 @@
+/*
+ * petla_cw4.cpp
+ *
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int i =12;
+    for(int i=12; i<100; i+=6)
+     { cout << i << endl; }
+
+	return 0;
+}
+
