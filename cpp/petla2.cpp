99e035e0bab0f8c43db085d81d0ef9575ed5b82b
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-10-02 19:37:28 +0200

    choinka pustak, hej kinga!

---
+++ cpp/petla2.cpp
@@ -0,0 +1,66 @@
+/*
+ * petla2.cpp
+ *
+ */
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void drukujTabliczke(int x, int y) {
+    int i, j;
+    for (i = 1; i <= x; i++) {
+        for (j = 1; j <= y; j++) {
+                 cout << setw(4) << i * j << " " ;
+        }
+    cout << endl;
+        }
+}
+void prostokat(int x, int y) {
+    int i, j ;
+    for (i = 1; i <= x; i++) {
+    for (j = 1; j <= y; j++)
+               { cout << "#" ; }
+
+    cout << endl;
+        }
+    }
+void pustak(int x, int y) {
+    int i, j ;
+    for (i = 1; i <= x; i++) {
+    for (j = 1; j <= y; j++) {
+        if (i == 1 || i == x)
+        cout<< "#";
+        else if (j == 1 || j ==y)
+        cout << "#";
+        else
+        cout <<" ";
+            }
+    cout << endl;
+        }
+    }
+void choinka(int x){
+    cout<<"Podaj x:";
+    cin>>x;
+    cout<<endl;
+  int i,j;
+  for (i=1;i<=x;i++)
+    {
+      for (j=1;j<=i;j++){
+        cout << "#";}
+      cout << endl;
+} }
+
+int main(int argc, char **argv)
+{
+    pustak(12,12);
+    cout<<endl;
+    prostokat(12,12);
+    cout<<endl;
+    choinka(1);
+    cout<<endl;
+	return 0;
+
+}
+
