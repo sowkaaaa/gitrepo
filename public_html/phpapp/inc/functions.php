2d7d43b23d81a9e7bc8f4213e06b67f6373ed660
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-10-11 09:40:50 +0200

    phpapp

---
+++ public_html/phpapp/inc/functions.php
@@ -0,0 +1,11 @@
+<?php
+function get_page_content($id){
+	if (file_exists($id.'.html'))
+		include($id.'.html');
+	else
+		include('404.html');
+
+
+}
+
+?>
--- public_html/phpapp/index.php
+++ public_html/phpapp/index.php
@@ -1,9 +1,22 @@
 <?php
 
-phpinfo();
+define('DINC', 'inc/');
+define('DBASE', 'baza/');
+$dbfile = 'db.sqlite3';
+$db = null;
+require_once(DINC.'functions.php');
+require_once(DINC.'db.php');
+require_once(DINC.'users.php');
+
+if (isset($_GET['id']))
+	$id=$_GET['id'];
+else
+	$id='witam';
+
+include_once(DINC.'template.php');
 
 ?>
 
 public.html
-localhost/~kl2ag1
+localhost/~kl2ag1/phpapp
 
