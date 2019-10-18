33a1d7ece0f73afe4d840b3be810578f2c38b632
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-10-18 09:41:17 +0200

    phpapp :))))

--- phpapp/inc/db.php
+++ phpapp/inc/db.php
@@ -1,3 +1,12 @@
 <?php
+function init_baza(){
+	global $db, $dbfile;
+	if (!file_exists(DBASE.$dbfile)){
+		$kom[] = 'Brak pliku bazy. Tworzę nowy.';
+		$db = new PDO("sqlite:DBASE.$dbfile");
+	}
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 ;
+
 ?>
