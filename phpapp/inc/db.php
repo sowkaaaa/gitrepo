a1fadf28ea856853917c92b63ae5188aa235a776
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-10-23 09:42:09 +0200

    ;)

--- phpapp/inc/db.php
+++ phpapp/inc/db.php
@@ -1,12 +1,20 @@
 <?php
 function init_baza(){
-	global $db, $dbfile;
-	if (!file_exists(DBASE.$dbfile)){
-		$kom[] = 'Brak pliku bazy. Tworzę nowy.';
+	global $db, $dbfile, $kom;
+	if (!file_exists($dbfile)) $kom[] = 'Brak pliku bazy. Tworzę nowy.';
+	
 		$db = new PDO("sqlite:DBASE.$dbfile");
-	}
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-;
 
+
+function init_tables(){
+	global $db;
+	if (file_exists(DBASE.'baza.sql')){
+		$sql = file_get_contents(DBASE.'baza.sql', 'r');
+		if ($db->exec($sql));{
+			$kom[] = "Utworzono tabelę! ";
+		}
+	}
+}
 ?>
--- phpapp/index.php
+++ phpapp/index.php
@@ -1,5 +1,4 @@
 <?php
-
 setlocale(LC_ALL, 'pl_PL.UTF-8');
 date_default_timezone_set('Europe/Warsaw');
 error_reporting(E_ALL);
@@ -9,11 +8,15 @@ ini_set('error_log', 'errorlog.txt');
 
 define('DINC', 'inc/');
 define('DBASE', 'db/');
-$dbfile = 'baza.db';
+$dbfile = DBASE.'baza.db';
 $db=null;
+$kom= array();
 require_once(DINC.'functions.php');
 require_once(DINC.'db.php');
 require_once(DINC.'users.php');
+
+init_baza();
+init_tables();
 
 if (isset($_GET['id']))
 	$id=$_GET['id'];
