5ab774fb5fb24121b3b9baa5954223497c573445
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-10-26 18:24:55 +0200

    tabliczka mnożenia

--- cpp/tablica2w.cpp
+++ cpp/tablica2w.cpp
@@ -1,6 +1,6 @@
 /*
  * tablica2w.cpp
- * 
+ *
  */
 // CZAS UNIKSOWY - liczba sekund, która upłynęła od 1 stycznia 1970r
 
@@ -8,21 +8,21 @@
 #include <iomanip>
 using namespace std;
 
-#define	N	10
-#define	M	10
+#define	N	11
+#define	M	11
 
 int main(int argc, char **argv)
 {
 	int tab2W[N][M];
 	int i, j;
-	
-	srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
-	
-	for (i =+ 1; i < N; i++){
+
+	//srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+
+	for (i = 1; i < N; i++){
 		// cout << "------->Tablica " << i << "<--------"  << endl;
-		for (j =+ 1; j < M; j++) {
+		for (j = 1; j < M; j++) {
 			// cout << i << "-" << j << endl;
-			tab2W[i][j] = rand() % 10;
+			tab2W[i][j] = i * j;
 			cout << setw(4) << tab2W[i][j] << " ";
 			}
 			cout << endl;
