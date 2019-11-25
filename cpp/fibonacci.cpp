e2c8d39f30f8d3b11e15bd60d03fa9a60a7cb9c7
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-11-25 10:36:15 +0100

    horner i reszta :)))

--- cpp/fibonacci.cpp
+++ cpp/fibonacci.cpp
@@ -1,31 +1,37 @@
 /*
  * fibonacci.cpp
- *  
  */
+
 
 #include <iostream>
 using namespace std;
 
-int fibonacci_it(int n){
-	int a = 0; // Fn-1
-	int b = 1; // Fn-2
-	int wynik = 0;
-	if (n == -2) return 0;
-	if (n == -1) return 1;
-	
-	for (int i = 2; i < n; i++){
-		wynik = a + b; 	// 1 // 2 // 3 
-		b = a; 			// 1 // 1 // 2
-		a = wynik; 		// 1 // 2 // 3
-	}
-	return wynik;
+int fibonacci_it(int n) {
+    int a = 0; // Fn-1
+    int b = 1; // Fn-2
+    int wynik = 0;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    for (int i=2; i<n; i++) {
+        wynik = a + b; //1//2//3
+        b = a; //1//1//2
+        a = wynik; //1//2//3
+    }
+    return wynik;
 }
 
 int main(int argc, char **argv)
 {
-	 for (int i = 0; i < 20; i++){
-		cout << i << ": fib(" << fibonacci_it(i-2) << ", " << fibonacci_it(i-1) << ") =" << fibonacci_it(i) << endl;
-	}
-	return 0;
+    cout << fibonacci_it(0) << endl;
+    cout << fibonacci_it(1) << endl;
+    
+    for (int i=2; i < 20; i++) {
+        cout << i << ": fib(" << fibonacci_it (i-2)
+             << ", " << fibonacci_it (i-1) << ") = "
+             << fibonacci_it (i) << "\t"
+             << (float)fibonacci_it(i) / (float)fibonacci_it(i-1)
+             << endl ;
+    }
+    return 0;
 }
 
---
+++ cpp/reszta.cpp
@@ -0,0 +1,20 @@
+/*
+ * reszta.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
+R = 69;
+1) R: = 60 - 50 * (69 % 50) = 19
+2) R: 19 - 10 * (19 % 10) = 9
+3) R: 9 - 5 * (9 % 5) = 4
+4) R: 5 - 2 
