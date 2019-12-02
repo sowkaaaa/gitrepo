642752e2e022b4efcdc070619405e92fc5ef8af6
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-12-02 06:16:03 +0100

    niestety ale nie tym razem :)

--- cpp/reszta.cpp
+++ cpp/reszta.cpp
@@ -1,76 +1,81 @@
 /*
- * reszta.cpp 
+ * reszta.cpp
  */
- 
+
+
 #include <iostream>
-using namespace std;
+using  namespace std;
 
 int nominaly[100];
 int liczbaNominalow;
 
-void wczytajNominaly(){
-	cout << "Podaj liczbe nominałów: ";
+void wczytajNominaly() {
+	cout << "Podaj liczbę nominałów: ";
 	cin >> liczbaNominalow;
-	
-	cout << "Podaj nominały w kolejnosci malejącej: " << endl;
+
+	cout << "Podaj nominały w kolejności malejącej: " << endl;
 	int i = 0;
 	int nominal = 0;
 	cin >> nominal;
 	nominaly[i] = nominal;
 	i++;
-	while(i < liczbaNominalow){
+	while (i < liczbaNominalow) {
 		cin >> nominal;
-		if (nominal > nominaly[i-1]) { 
-			cout << "Nominał wiekszy od poprzedniego!" << endl;
+		if (nominal > nominaly[i-1]) {
+			cout << "Nominał większy od poprzedniego!" << endl;
 			continue;
 		}
 		nominaly[i] = nominal;
 		i++;
-	}
-
-	//for(int i = 0; i < liczbaNominalow; i++){
+	};
+	//for(int i = 0; i < liczbaNominalow; i++) {
 		//cin >> nominaly[i];
 	//}
-	for(int i = 0; i < liczbaNominalow; i++){
-		cout << nominaly[i] << " "; }
+	for(int i = 0; i < liczbaNominalow; i++) {
+		cout << nominaly[i] << " ";
 	}
+	cout << endl;
+}
 
-
-int znajdzReszte(int reszta){
+int znajdzReszte(int reszta) {
 	cout << "Wypłacone nominały: " << endl;
 	int aktualnyNominal = 0;
-	
-	while(reszta > 0 && aktualnyNominal < liczbaNominalow){
+
+	while (reszta > 0 && aktualnyNominal < liczbaNominalow) {
 		cout << "reszta = " << reszta << endl;
-		znajdz najwiekszy dostepny nominal mniejszy ood reszt;
-		cout << "aktualny nominał: " << nominaly[aktualnyNominal] << endl;
-		oblicz wymagana ilosc nominalów
-		pomniejsz reszte
-		
+		//znajdź największy dostępny nominał mniejszy od reszty; if (reszta >= nominaly[i])
+		cout << "aktualny nominał: " << nominaly[aktualnyNominal] >> endl;
+		// oblicz wymaganą ilość nominałów int L; L = reszta / nominaly[i];
+		// pomniejsz resztę reszta = reszta -(nominaly[i]*L)
+		//brak nominalow do wydanie i++
 	}
-	
+	if (reszta > 0)
+		cout << "Brak nominałów do wydania kwoty" << reszta << endl;
+
 }
-// [50, 50, 50, 20, 10, 10, 5, 5, 5, 1]
+// [50, 50, 20, 20, 10, 10, 5, 5, 5, 1]
+
 int main(int argc, char **argv)
 {
 	wczytajNominaly();
 	int cena;
 	int wplata;
-	cout << "Podaj wpłatę: ";
-	cin >> wplata;
-	
-	do { // petla blokująca
-		cout << "POdaj wpłatę: ";
+	cout << "Podaj cenę: ";
+	cin >> cena;
+
+	do { //pętla blokująca
+		cout << "Podaj wpłatę: ";
 		cin >> wplata;
-	} while(wplata < cena);
-	
+	} while (wplata < cena);
+
 	znajdzReszte(wplata - cena);
-	
+
 	return 0;
 }
 
-// R = 69;
-// 1) R: = 69 - 50 * (69 % 50) = 19
-// 2) R: 19 - 10 * (19 % 10) = 9
-// 3) R: 9 - 5 * (9 % 5) = 4
-// 4) R: 5 - 2 * (5 % 2) = 1 
+/*R = 69;
+69 % 50 = 1
+1) R: 60 - 50 * (69 % 50) = 19
+2) R: 19 - 10 * (19 % 10) = 9
+3) R: 9 - 5 * (9 % 5) = 4
+4) R: 4 - 2 * (4 % 2) = 0 */
