1e378a2f5a2ac5729e7c479e565063eb3d0d5806
sowkaaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-12-11 21:02:49 +0100

    witam i o zdrowie pytam

--- cpp/euklides.cpp
+++ cpp/euklides.cpp
@@ -1,43 +1,57 @@
 /*
  * euklides.cpp
- * 
+ *
  */
-
 #include <iostream>
 using namespace std;
 
-
-// NWD(a, b) = b dla a = 0
-// NWD(a, b) = { a = a % b; b = b - a; }
-// NWD(a, b) = {a % b; b - a; }
-
+// NWD(a,b) = b dla a = 0
+// NWD(a,b) = {a = a % b; b = b - a; }
 int NWD_re1(int a, int b) {
-		if (a != 0)
-			return NWD_re1(a % b, b - (a % b));
-		else
-		 return b;	
+	if (a != 0)
+		return NWD_re1(a%b, b - (a % b));
+	else
+		return b;
 }
-// NWD(a, b) = a dla b= 0
-// NWD(a, b) = {a = b dla b = a % b }
-// NWD(a, b) = NWD{b, a % b}
+// NWD(a,b) = a dla b = 0;
+// NWD(a,b) = {a=b, b = a % b}
+// NWD(a,b) = NWD(b, a % b)
+int NWD_re2(int a, int b) {
+	if (b != 0)
+		return NWD_re2(b, a % b);
+	else
+		return a;
+}
 
-int NWD_re1(int a, int b) {
-		if (b != 0)
-			return NWD_re2(b ,a % b);
-		else
-		 return a;	
-	 }
-	 // napisz literacyjna wersje funkcji NWD_re2
+int NWD_it2(int a, int b) {
+	int i;
+	a = b = i = 0;
+	cout << "Podaj dwie liczby całkowite: " << endl;
+	cin >> a >> b;
+	while (b > 0) {
+		i++;
+        a = b;
+		b = a % b;
+	}
+	cout << "NWD: " << a << endl;
+}
 
 int main(int argc, char **argv)
 {
-	int a,b,i;
+	int a, b, i;
 	a = b = i = 0;
-	cout << " Podaj dwie liczby całkowite: " << endl;
+	cout << "Podaj dwie liczby całkowite: " << endl;
 	cin >> a >> b;
-	cout << NWD_re1(a, b); << endl;
-	cout << NWD_re2(a, b); << endl;
+	while (a > 0) {
+		i++;
+		a = a % b;
+		b = b - a;
+	}
+	cout << "NWD: " << b << endl;
+	cout << "Powtórzeń: " << i << endl;
 
+	cout << NWD_re1(a, b) << endl;
+	cout << NWD_re2(a, b) << endl;
+	cout << NWD_it2(a, b) << endl;
 	return 0;
 }
-
