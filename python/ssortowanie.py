041f87bc7824aec571120cfffdcf3d867514a983
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2019-12-19 11:40:29 +0100

    python come back

--- python/ssortowanie.py
+++ python/ssortowanie.py
@@ -2,23 +2,50 @@
 # -*- coding: utf-8 -*-
 #
 #  ssortowanie.py
-from random import randint
+
+
+from random import randint 
 
 def wypelnij(lista, n):
-	for i in range(n):
-		lista.append(randint(0, 100))
+    for i in range(n):
+        lista.append(randint(0, 100))
 
 def wyswietl(lista):
-	for i in lista:
-		print(i, end='')
-	print()
+    for i in lista:
+        print(i, " ", end='')
+    print()
+ 
+def zamien(tab, j, i):
+	tmp = tab[j]
+	tab[j] = tab[i]
+	tab[i] = tmp
+    
+def sort_bubble(tab):
+	n = len(tab)
+	for i in range(n-1, 0, -1):
 		
-def main(args):
-	n = int(input('Ile wylosować liczb?'))
-	lista = []
-	wypelnij(lista, n)
-	wyswietl(lista)
+		for j in range(0, i):
+			if tab[j] > tab[j+1]:
+				zamien(tab, j, j+1)
+				
+
+def sort_selection(tab):
+	n = len(tab)
+	for i in range(0, n):
+		for j in range(i+1
+			if tab[j] = tab[i]:
+				zamien(tab, j, i)
+
 	
+def main(args):
+    n = int(input('Ile liczb wylosować? '))
+    lista = []
+    wypelnij(lista, n)
+    wyswietl(lista)
+    sort_bubble(lista)
+    wyswietl(lista)
+    sort_selection(lista)
+    wyswietl(lista)
     return 0
 
 if __name__ == '__main__':
