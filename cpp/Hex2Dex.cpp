6e975ac8a6967d2ab11557c5208c7651103f7dd3
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2020-02-10 10:35:59 +0100

    SYSTEM 16 sprawdziansko

---
+++ cpp/Hex2Dex.cpp
@@ -0,0 +1,19 @@
+/*
+ * Hex2Dex.cpp
+ */
+#include <iostream>
+using namespace std;
+
+int Hex2Dex(char tab){
+	int x = strlen(tab) 
+	for(int i = x-1, i >= 0; i--)
+	}
+
+int main(int argc, char **argv)
+{	int p = 4
+	int tab[p]
+	cout << "Podaj czteroznakową liczbę szesnastkową: ";
+	cin >> tab;
+	return 0;
+}
+
