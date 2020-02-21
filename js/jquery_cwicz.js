e77da6b2200cc1325863fa743aa623c2f0e293d0
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2020-02-21 10:35:02 +0100

    JQUERY kolorkiii

--- js/jquery_cwicz.js
+++ js/jquery_cwicz.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
   $(".wynik").text("Nowa treść");
 	console.log(($("p").text()));
-  $("#przycisk").click(function()){
-    ;
+  $("#przycisk").click(function(){
+  var vnk = parseInt($("#vnk").val());
+  var nk = parseInt($("#nk").val());
+  var R = vnk / nk * 100;
+  var div2 = '<div class="wspolczynnik">'+R+'</div>';
+  $("div.wynik").after(div2);
   });
+  $("input").mouseover(function(){
+    $(this).addClass("tloBlue");
+  });
+  $("input").mouseleave(function(){
+    $(this).removeClass("tlo");
+});
 });
