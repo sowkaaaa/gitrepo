5066f009e669f605967362da75fe8cccf065d977
sowkaaaa <michal.sowinski@lo1.sandomierz.pl>
2020-03-11 09:39:42 +0100

    zabawa fajna

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  // put drawing code here
+	noFill();
+  strokeWeight(10);
+  stroke('blue');
+  circle(200, 200, 200);
+  stroke('black');
+  circle(430, 200, 200);
+  stroke('red');
+  circle(630, 200, 200);
+  stroke('green');
+  circle(215, 320, 200);
+  stroke('yellow');
+  circle(545, 320, 200);
+
+
+
+
+}
